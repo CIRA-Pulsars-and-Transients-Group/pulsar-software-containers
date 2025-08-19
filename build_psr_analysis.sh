48efaa1838243bb0da2e20eaf2321d91769bfaf9
Bradley Meyers <bradley.meyers1993@gmail.com>
2025-08-19 17:14:57 +0800

    allow layer/cache re-use when building

--- build_psr_analysis.sh
+++ build_psr_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker buildx build --no-cache --progress=plain -f psr-analysis.dockerfile -t psr-analysis:$(date -I) .
-#docker buildx build --progress=plain -f psr-analysis.dockerfile -t psr-analysis:$(date -I) .
+#docker buildx build --no-cache --progress=plain -f psr-analysis.dockerfile -t psr-analysis:$(date -I) .
+docker buildx build --progress=plain -f psr-analysis.dockerfile -t psr-analysis:$(date -I) .
 
 docker image tag psr-analysis:$(date -I) cirapulsarsandtransients/psr-analysis:$(date -I)
 docker image tag cirapulsarsandtransients/psr-analysis:$(date -I) cirapulsarsandtransients/psr-analysis:latest
