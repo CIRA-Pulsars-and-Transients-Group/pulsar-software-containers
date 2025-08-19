664d9aa000a06ea466aa8e83cda7158b2b08a0fb
Bradley Meyers <bradley.meyers1993@gmail.com>
2025-08-19 17:11:34 +0800

    streamline docker image tagging to make uploads easier

--- build_psr_search.sh
+++ build_psr_search.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker buildx build --progress=plain -f psr-search.dockerfile -t psr-search:$(date -I) .
+docker buildx build --no-cache --progress=plain -f psr-search.dockerfile -t psr-search:$(date -I) .
+#docker buildx build --progress=plain -f psr-search.dockerfile -t psr-search:$(date -I) .
+
+docker image tag psr-search:$(date -I) cirapulsarsandtransients/psr-search:$(date -I)
+docker image tag cirapulsarsandtransients/psr-search:$(date -I) cirapulsarsandtransients/psr-search:latest
